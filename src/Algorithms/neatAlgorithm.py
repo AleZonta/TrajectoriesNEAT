remove saving data not needed for computation fixed loading data from arguments.py settings

src/Algorithms/neatAlgorithm.py
@@ -37,22 +37,18 @@ from src.Helpers.APF import LoadAPF
 from src.Helpers.Division.ComputeDivision import SubMatrix
 from src.Helpers.GenomePhenome import GenomeMeaning
 
-from src.Settings.utils import args
+from src.Settings.arguments import args
 
 
 def eval_genomes(genome, config):
     number_of_tra_to_generate = args.numb_of_tra
 
-    root = os.path.dirname(os.path.abspath(__file__))
-    path = root.replace("Algorithms", "") + "/Data/"
-    loader_apf = LoadAPF(path="{}/the_right_one_fast".format(path),
+    loader_apf = LoadAPF(path="{}/the_right_one_fast".format(args.data_directory),
                          logger=None)
     loader_apf.load_apf_only_routing_system()
     loader_apf.match_index_with_coordinates()
 
-    root = os.path.dirname(os.path.abspath(__file__))
-    path = root.replace("Algorithms", "") + "/Data/"
-    real_tra_train = np.load("{}/real_tra_train_starting_points.npy".format(path), allow_pickle=True)
+    real_tra_train = np.load("{}/real_tra_train_starting_points.npy".format(args.data_directory), allow_pickle=True)
 
     net = neat.nn.FeedForwardNetwork.create(genome, config)
 
@@ -64,7 +60,7 @@ def eval_genomes(genome, config):
                            values_matrix=(loader_apf.x_values, loader_apf.y_values))
     sub_matrix.divide_into_cells()
 
-    fitness_landscape = pickle.load(open("{}/3d_fitness_in_2d_with_limitation.pickle".format(path), 'rb'))
+    fitness_landscape = pickle.load(open("{}/3d_fitness_in_2d_with_limitation.pickle".format(args.data_directory), 'rb'))
 
     # fitness_total = []
     # behaviour_total = []
@@ -170,7 +166,7 @@ class neatAlgorithm(object):
             self._log.debug("Error occurred in testing the winning genome. Operation Aborted")
             self._log.debug(e)
         try:
-            self.test_winning_genome_attraction_scales(individual=winner)
+            self.test_winning_genome_attraction_scales(individual=winner, output_dir=self._output_directory)
         except Exception as e:
             self._log.debug("Error occurred in testing the attraction. Operation Aborted")
             self._log.debug(e)

src/Fitness/GeneralFitness.py
@@ -24,8 +24,8 @@ from scipy.spatial import distance
 import itertools
 
 from src.Fitness.ValueGraphFitness import VAL_NO_DATA, LIMIT_TIMESTEPS, convert, MAX_FITNESS, MAX_TOTAL_FITNESS, \
-    get_fitness_value
-from src.Helpers.Funcs import list_neighbours
+    get_fitness_value, get_next_point
+from src.Helpers.Funcs import list_neighbours, _get_direction
 from src.Helpers.Point import Point
 
 PRE_DEFINED_BEHAVIOURS_ALL = list(set(itertools.permutations([-1, -1, -1, -1, -0.011, 1])))
@@ -129,13 +129,6 @@ def compute_general_fitness(net, number_to_generate, real_tra, apf, sub_matrix, 
                     all_the_charges.extend([float(VAL_NO_DATA) for _ in range(6)])
                     final_with_additional.append(VAL_NO_DATA)
             all_the_charges.extend(final_with_additional)
-
-            # all_the_charges = []
-            # for p in points:
-            #     if p in points_on_the_street:
-            #         all_the_charges.append(1)
-            #     else:
-            #         all_the_charges.append(VAL_NO_DATA)
 
             # add count of timesteps already moved
             all_the_charges.append(convert(old_max=LIMIT_TIMESTEPS, old_min=0,

src/Helpers/Division/CollectionCells.py
@@ -18,6 +18,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import numpy as np
 import warnings
 
+from src.Settings.arguments import args
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import os
 
@@ -59,14 +61,10 @@ class CollectionCells(object):
         """
         if not self._save_and_store:
             return False
-        root = os.path.dirname(os.path.abspath(__file__))
-        output_folder = root.replace("Helpers/Division", "Data")
         # output_folder = "/Volumes/Cthulhu/MacDataPycharmProjects/GTEA/Data/Division/"
         name_file = "{}_{}_by_{}_dict_version".format(name, self._x_division, self._y_division)
-        if os.path.isfile("{}/{}.pickle".format(output_folder, name_file)):
-            self._list_cells = pickle.load(open("{}/{}.pickle".format(output_folder, name_file), 'rb'))
-
-
+        if os.path.isfile("{}/{}.pickle".format(args.data_directory, name_file)):
+            self._list_cells = pickle.load(open("{}/{}.pickle".format(args.data_directory, name_file), 'rb'))
             return True
         return False
 
@@ -127,10 +125,8 @@ class CollectionCells(object):
         #         return cell
 
     def load_mmap_data(self, not_normalised=False):
-        root = os.path.dirname(os.path.abspath(__file__))
-        output_folder = root.replace("Helpers", "Data").replace("Division", "")
         if not_normalised:
-            name_file = "{}/cell_data_to_mmap.dat".format(output_folder)
+            name_file = "{}/cell_data_to_mmap.dat".format(args.data_directory)
 
             data_input = np.memmap(name_file, dtype='float32', mode='r', shape=(154, 155, 6, 2, 1600))
             # data_input = np.nan_to_num(data_input)
@@ -138,7 +134,7 @@ class CollectionCells(object):
             self.max_values = 62230.51
             # data_input = scale(data_input, -1, 1)
         else:
-            name_file = "{}/cell_data_to_mmap_normalised.dat".format(output_folder)
+            name_file = "{}/cell_data_to_mmap_normalised.dat".format(args.data_directory)
             data_input = np.memmap(name_file, dtype='float32', mode='r', shape=(154, 155, 6, 2, 1600))
         count = 0
         for k, cell in self._list_cells.items():
@@ -146,7 +142,7 @@ class CollectionCells(object):
             cell.index = count
             count += 1
 
-        name_file = "{}/indexing_fast.dat".format(output_folder)
+        name_file = "{}/indexing_fast.dat".format(args.data_directory)
         ndexing = np.memmap(name_file, dtype='int16', mode='r', shape=(6159, 6083, 2))
         for k, cell in self._list_cells.items():
             cell._indexing = ndexing

src/Helpers/Division/ComputeDivision.py
@@ -20,12 +20,11 @@ import sys
 import os
 
 from haversine import haversine
-# import seaborn as sns
 
 import numpy as np
-import warnings
 
-warnings.simplefilter(action='ignore', category=FutureWarning)
+from src.Settings.arguments import args
+
 from src.Helpers.Division.CollectionCells import CollectionCells
 
 
@@ -39,9 +38,7 @@ class SubMatrix(object):
         self._save_and_store = save_and_store
         self._values_matrix = values_matrix
 
-        root = os.path.dirname(os.path.abspath(__file__))
-        data_folder = root.replace("Helpers/Division", "Data")
-        name_file = "{}/matrix_id_matrix_mmap.dat".format(data_folder)
+        name_file = "{}/matrix_id_matrix_mmap.dat".format(args.data_directory)
         self._coordinate_index = np.memmap(name_file, dtype='int8', mode='r', shape=(6159, 6201, 2))
 
     def divide_into_cells(self, x_division=40, y_division=40, not_normalised=False):
@@ -73,8 +70,6 @@ class SubMatrix(object):
 
         if self._log is not None:
             self._log.debug("Point division loaded from file")
-
-
 
         self._list_of_cells.load_mmap_data(not_normalised=not_normalised)
         #

src/Helpers/GenomePhenome.py
@@ -25,6 +25,8 @@ from pathlib import Path
 import numpy as np
 from tqdm import trange
 
+from src.Settings.arguments import args
+
 
 class GenomeMeaning(object):
     """
@@ -63,8 +65,7 @@ class GenomeMeaning(object):
         :return:
         """
         # loading coordinates
-        root = os.path.dirname(os.path.abspath(__file__))
-        phenotype_file = root.replace("Helpers", "") + "/Data/Phenotype"
+        phenotype_file = args.data_directory + "/Phenotype"
         with open(phenotype_file, 'r') as f:
             self._types = json.load(f)
         if self._log is not None:
@@ -86,63 +87,17 @@ class GenomeMeaning(object):
 
             self._log.warning("Code commented for performances, enable it if needed")
 
-        root = os.path.dirname(os.path.abspath(__file__))
-        mmap_file = root.replace("Helpers", "") + "Data/name_and_position.dat"
-        if os.path.isfile(mmap_file):
-            if self._log is not None:
-                self._log.debug("mmap file exist, loading it")
+        mmap_file = args.data_directory + "name_and_position.dat"
 
-            self.name_and_position = np.memmap(mmap_file, dtype='float32', mode='r', shape=(56, 938737, 2))
+        self.name_and_position = np.memmap(mmap_file, dtype='float32', mode='r', shape=(56, 938737, 2))
 
-            with open('{}Data/order_on_mmap.pickle'.format(root.replace("Helpers", "")), 'rb') as handle:
-                self._order_name_and_position = pickle.load(handle)
+        with open('{}/order_on_mmap.pickle'.format(args.data_directory), 'rb') as handle:
+            self._order_name_and_position = pickle.load(handle)
 
-            with open('{}Data/name_typology.pickle'.format(root.replace("Helpers", "")), 'rb') as handle:
-                self.name_typologies = pickle.load(handle)
-            self.method_mmap = True
-        else:
-            self.name_and_position = {}
-            for name in self.name_typologies:
-                lowercase_name = name.lower()
-                root = os.path.dirname(os.path.abspath(__file__))
-                data_file = root.replace("Loaders", "") + "/Data/"
-                if test:
-                    name_file = data_file + lowercase_name + "_test.csv"
-                else:
-                    name_file = data_file + lowercase_name + ".csv"
-                my_file = Path(name_file)
-                if my_file.is_file():
+        with open('{}/name_typology.pickle'.format(args.data_directory), 'rb') as handle:
+            self.name_typologies = pickle.load(handle)
+        self.method_mmap = True
 
-                    list_word_accepted = self.name_and_details[lowercase_name]
-                    list_word_accepted_fixed = []
-                    for word in list_word_accepted:
-                        if word == "others":
-                            word = "others_" + name
-                        list_word_accepted_fixed.append(word)
-                        self.link_main_obj_and_small_objs.update({word: name})
-                    self.list_of_names_per_genome.extend(list_word_accepted_fixed)
-
-                    # file exist
-                    with open(name_file) as csvfile:
-                        reader = csv.DictReader(csvfile)
-                        dic = {}
-
-                        for row in reader:
-                            name_element = row["names"].lower()
-
-                            if name_element in list_word_accepted:
-                                okay_id = name_element
-                            else:
-                                okay_id = "others_" + name
-
-                            if row["x"] != "":
-                                dic.setdefault(okay_id, []).append((float(row["x"]), float(row["y"])))
-
-                        self.name_and_position.update({lowercase_name: dic})
-                else:
-                    if self._log is not None:
-                        self._log.debug("File {} not present in folder. Please provide it".format(name_file))
-                    raise ValueError("File {} not present in folder. Please provide it".format(name_file))
 
         if self._log is not None:
             self._log.info("Coordinates loaded!")

src/Main.py
@@ -27,9 +27,20 @@ import mlflow
 from src.Algorithms.neatAlgorithm import neatAlgorithm
 from src.Fitness.GeneralFitness import PRE_DEFINED_BEHAVIOURS_ALL
 from src.Fitness.ValueGraphFitness import _get_max_fitness_possible
-from src.Settings.utils import args
+from src.Settings.arguments import args
 
 if __name__ == "__main__":
+    p_distance = "point_distance_"
+    if args.point_distance is None:
+        p_distance = "point_distance_none"
+    else:
+        if 0 in args.point_distance:
+            p_distance += "0_"
+        if 1 in args.point_distance:
+            p_distance += "1_"
+        if 2 in args.point_distance:
+            p_distance += "2_"
+
     end_output = "{}/{}_{}{}/".format(args.output_directory, args.name_experiment, p_distance,
                                       args.fitness_definition)
     if not os.path.isdir(end_output):
@@ -37,6 +48,7 @@ if __name__ == "__main__":
 
     name_log = end_output + "/" + args.name_experiment
 
+    # -------------- logger
     logger = logging.getLogger(name_log)
     logger.setLevel(logging.DEBUG)
     # create console handler with a higher log level
@@ -53,38 +65,27 @@ if __name__ == "__main__":
     fh.setFormatter(formatter)
     # add the handlers to the logger
     logger.addHandler(fh)
+    # -------------- logger
 
     logger.info(args)
 
+    # -------------- mlflow
     mlflow.set_experiment(experiment_name=args.name_experiment)
     mlflow.start_run(run_name=args.run_name)
+    # -------------- mlflow
+
     # seed for reproducibility
     np.random.seed(args.seed)
     tf.compat.v1.set_random_seed(args.seed)
     random.seed(a=args.seed)
+
+    # -------------- mlflow store settings
     mlflow.log_param("seed", args.seed)
-
-    p_distance = "point_distance_"
-    if args.point_distance is None:
-        p_distance = "point_distance_none"
-    else:
-        if 0 in args.point_distance:
-            p_distance += "0_"
-        if 1 in args.point_distance:
-            p_distance += "1_"
-        if 2 in args.point_distance:
-            p_distance += "2_"
-
-
     mlflow.log_param("numb_of_tra", args.numb_of_tra)
-    mlflow.log_param("k", args.k)
     mlflow.log_param("checkpoint", args.checkpoint)
-    mlflow.log_param("prob_add", args.prob_add)
     mlflow.log_param("fitness_definition", args.fitness_definition)
     mlflow.log_param("point_distance", args.point_distance)
     mlflow.log_param("random_point_start", args.random_point_start)
-    mlflow.log_param("neat", args.neat)
-    mlflow.log_param("two_stages", args.two_stages)
 
     max_fitness_possible = _get_max_fitness_possible(fitness_definition=args.fitness_definition)
     mlflow.log_param("max_fitness_possible", max_fitness_possible)
@@ -93,21 +94,13 @@ if __name__ == "__main__":
     if args.numb_of_tra > len(PRE_DEFINED_BEHAVIOURS_ALL):
         raise ValueError("Too many trajectories, not enough behaviours")
 
-    local_dir = os.path.dirname(__file__)
-    name_config = "config-neat-novelty"
-    config_path = os.path.join("/Users/alessandrozonta/PycharmProjects/deapGeneration/Settings/", name_config)
-    if not os.path.exists(config_path):
-        config_path = os.path.join(local_dir + "/Settings/", name_config)
-        if not os.path.exists(config_path):
-            config_path = os.path.join(local_dir + "Settings/", name_config)
-
-    a = neatAlgorithm(mlflow=mlflow, config_file=config_path, logger=logger, output=end_output,
-                      prob_add=args.prob_add, fitness_definition=args.fitness_definition)
-    a.initialise(frequency_checkpoints=args.freq_checkpoints, restore_checkpoint_name=args.checkpoint)
+    a = neatAlgorithm(mlflow=mlflow, config_file=args.config_path, logger=logger, output=end_output,
+                      fitness_definition=args.fitness_definition)
+    check = None if args.checkpoint == "" else args.checkpoint
+    a.initialise(frequency_checkpoints=args.freq_checkpoints, restore_checkpoint_name=check)
 
     a.run(generations=args.generations)
 
     logging.info("End program")
 
     mlflow.end_run()
-